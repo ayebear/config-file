70eda6faa5637566127d84cd92dc22e5b19703a6
git <ayebear@gmail.com>
2014-03-31 00:43:03 -0400

    Fixed include typo

--- file.cpp
+++ file.cpp
@@ -1,7 +1,7 @@
 // See the file COPYRIGHT.txt for authors and copyright information.
 // See the file LICENSE.txt for copying conditions.
 
-#include "File.h"
+#include "file.h"
 #include <fstream>
 #include <iostream>
 #include "strlib.h"
