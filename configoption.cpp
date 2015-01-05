be25079f687d7b27a3c522fc4ed5c866f751e863
ayebear <ayebear@gmail.com>
2015-01-05 00:01:22 -0500

    Updated copyright year

--- configoption.cpp
+++ configoption.cpp
@@ -1,5 +1,5 @@
-// See the file COPYRIGHT.txt for authors and copyright information.
-// See the file LICENSE.txt for copying conditions.
+// Copyright (C) 2014-2015 Eric Hebert (ayebear)
+// This code is licensed under GPLv3, see LICENSE.txt for details.
 
 #include "configoption.h"
 
